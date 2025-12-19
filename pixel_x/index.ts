2291c0185f6d364f2c438cbd3d4535a97eb42d66
Rainer Stropek <rainer@timecockpit.com>
2025-12-19 08:29:46 +0100

    Saved in TypeScript Playground at 2025-12-19T07:29:45.813Z

---
+++ pixel_x/index.ts
@@ -0,0 +1,15 @@
+function setup() {
+    const SIZE = 425; // Canvas size
+    const GRID = 25;  // Size of the squares
+
+    createCanvas(SIZE, SIZE);
+    background("black");
+
+    noStroke();
+
+    for (let i = 1; i <= 15; i += 1) {
+        fill("yellow")
+
+        rect(i * 25, i * 25, GRID, GRID)
+    }
+}
