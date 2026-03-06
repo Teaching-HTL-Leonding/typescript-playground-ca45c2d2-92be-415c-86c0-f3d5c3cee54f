373272de4aa8a4baeb456b9ea077075204630127
Rainer Stropek <rainer@timecockpit.com>
2026-03-06 10:05:39 +0100

    Saved in TypeScript Playground at 2026-03-06T09:05:39.181Z

--- tally_visualizer/index.ts
+++ tally_visualizer/index.ts
@@ -10,7 +10,7 @@ const COL_GAP = 10;   // gap between groups
 const ROW_GAP = 16;   // gap between rows
 const MARGIN = 16;
 const TITLE_HEIGHT = 30;
-const COLOR_STROKE ="black"
+const COLOR_STROKE = "black"
 const CELL_W = GROUP_W + COL_GAP;
 const CELL_H = GROUP_H + ROW_GAP;
 
@@ -23,22 +23,24 @@ let randomNumber: number;
 function isColored(row: number, col: number, n: number): boolean {
     return row * COL_GAP + col < n;
 }
-let strokes =0
+let strokes = 0
 
-function drawline(colored: boolean): void {
-   
+function drawline(colored:number): void {
+
     push();
     stroke(COLOR_STROKE);
     strokeWeight(2);
     fill(colored ? COLOR_FILLED : COLOR_EMPTY);
-   for(let i = 0;i<4;i++){
-    line(i*10,0,i*10,GROUP_H)
-   }
-   if (strokes===5){
-    line(0,GROUP_H,30,0,)
-   }
- translate (CELL_W,0)
-   groupIndex++}
+    for (let i = 0; i < colored; i++) {
+        line(i * 10, 0, i * 10, GROUP_H)
+    }
+    if (strokes === 5) {
+        line(0, GROUP_H, 30, 0,)
+    }
+    translate(CELL_W, 0)
+    groupIndex++
+    pop()
+    translate(0, CELL_H)
 }
 
 function setup(): void {
@@ -46,7 +48,7 @@ function setup(): void {
     const canvasH = ROWS * CELL_H + TITLE_HEIGHT + MARGIN;
     createCanvas(canvasW, canvasH);
 
- randomNumber = Math.floor(Math.random() * (TOTAL + 1));
+    randomNumber = Math.floor(Math.random() * (TOTAL + 1));
 
 
     background("white");
@@ -59,14 +61,14 @@ function setup(): void {
     text(`Random number: ${randomNumber} /500`, width / 2, 20);
 
 
-translate(MARGIN + CELL_W / 2, TITLE_HEIGHT + CELL_W / 2);
-for (let row = 0; row < ROWS; row++) {
-    push();
-    for (let col = 0; col < COL_GAP; col++) {
-        drawline(isColored(row, col, randomNumber));
-        translate(CELL_W, 0);
+    translate(MARGIN + CELL_W / 2, TITLE_HEIGHT + CELL_W / 2);
+    for (let row = 0; row < ROWS; row++) {
+        push();
+        for (let col = 0; col < COL_GAP; col++) {
+            drawline(5);
+            translate(CELL_W, 0);
+        }
+        pop();
+        translate(0, CELL_W);
     }
-    pop();
-    translate(0, CELL_W);
-}
 }
